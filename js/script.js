cbcc18748974255ef9c26c78e6fcb675892b6602
fishy52 <derfisch04@gmail.com>
2023-03-22 13:54:24 +0100

    sex5

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ let main_element = document.getElementById("main");
 
 function loadQuestion(question_id) {
     main_element.innerHTML = `
-        <h1></h1>
+        <h1>${questions[question_id].question}</h1>
+        <img src='${questions[question_id].image_path}'>
+        <div class="questions_grid">
+            <div class="questions_grid_item">
+                <h2>questions[question_id]</h2>
+            </div>
+        </div>
     `;
 }
