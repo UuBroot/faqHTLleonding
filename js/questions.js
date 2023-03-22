bcec2de7ba8fd331312f1449a6935a145cf07be2
aliiiiiii9 <alihahn@outlook.at>
2023-03-22 13:48:57 +0100

    sex3

--- js/questions.js
+++ js/questions.js
@@ -1,9 +1,44 @@
 let question1 = {
-    question: "Bist du Jungfrau?",
-    info_points: 10,
-    medien_points: 2,
+    question: "Interessierst du dich für den Menschlichen Körper?",
+    info_points: 0,
+    medien_points: 0,
     elektro_points: 0,
-    medizin_points: 0
+    medizin_points: 5,
+    image_path: '../img/body.jpg'
+}
+let question2 = {
+    question: "Interessierst du dich fürs Programmieren?",
+    info_points: 5,
+    medien_points: 3,
+    elektro_points: 1,
+    medizin_points: 0,
+    image_path: '../img/programming.jpg'
+}
+let question3 = {
+    question: "Interessierst du dich für Datenbanken?",
+    info_points: 4,
+    medien_points: 3,
+    elektro_points: 0,
+    medizin_points: 0,
+    image_path: '../img/progmming2.jpg'
+}
+let question4 = {
+    question: "",
+    info_points: ,
+    medien_points: ,
+    elektro_points: ,
+    medizin_points: ,
+    image_path: '../img/.jpg'
 }
 
+
+
+let question = {
+    question: "",
+    info_points: ,
+    medien_points: ,
+    elektro_points: ,
+    medizin_points: ,
+    image_path: '../img/.jpg'
+}
 let questions = [question1];
