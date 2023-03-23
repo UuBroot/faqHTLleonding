6be5d0d9cf948b9999a81d25d2475382c2b87345
fishy52 <derfisch04@gmail.com>
2023-03-23 10:59:28 +0100

    sex

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 let current_question = 0;
 let main_element = document.getElementById("main");
 let points = [0, 0, 0, 0]
-let percentages = [0, 0, 0, 0];
+let percentages = [
+    {
+        percentage: 0,
+        zweig: "Informatiker:in"
+    },
+    {
+        percentage: 0,
+        zweig: "Medientechniker:in"
+    },
+    {
+        percentage: 0,
+        zweig: "Elektrotechniker:in"
+    },
+    {
+        percentage: 0,
+        zweig: "Medizintechniker:in"
+    },
+    
+]
 let all_points = 0;
 
 loadQuestion(current_question);
@@ -31,19 +49,39 @@ function loadQuestion(question_id) {
     } else {
         //generate endscreen
 
-        
+
         for (let k = 0; k < points.length; k++) {
             all_points += points[k];
         }
 
-        
+
         for (let i = 0; i < points.length; i++) {
-            percentages[i] = all_points / points[i];
+            percentages[i].percentage = points[i] * 100 / all_points;
+        }
+
+        //BUBBLESORT
+        for (var i = 0; i < percentages.length; i++) {
+
+            // Last i elements are already in place 
+            for (var j = 0; j < (percentages.length - i - 1); j++) {
+
+                // Checking if the item at present iteration
+                // is greater than the next iteration
+                if (percentages[j].percentage > percentages[j + 1].percentage) {
+
+                    // If the condition is true then swap them
+                    var temp = percentages[j]
+                    percentages[j] = percentages[j + 1]
+                    percentages[j + 1] = temp
+                }
+            }
         }
 
         main_element.innerHTML = `
-        <h1>Du bist zu ${Math.max(percentages)}</h1>
-        
+        <h1>Du bist zu ${Math.round(percentages[3].percentage)}% ${percentages[3].zweig}</h1>
+        <h2>${Math.round(percentages[2].percentage)}% ${percentages[2].zweig}</h2>
+        <h2>${Math.round(percentages[1].percentage)}% ${percentages[1].zweig}</h2>
+        <h2>${Math.round(percentages[0].percentage)}% ${percentages[0].zweig}</h2>
     `;
     }
 }
