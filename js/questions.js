d601db7945f033cf2057b26abf03ffbf67bd6278
UuBroot <lolylyt@gmail.com>
2023-03-27 10:48:59 +0200

    formated document

--- js/questions.js
+++ js/questions.js
@@ -1,94 +1,113 @@
 //points: [Informatik, Medientechnik, Elektronik, Medizintechnik]
 
 let question1 = {
-    question: "Wie sehr interessierst du dich für den Menschlichen Körper?",
-    points: [0, 0, 0, 5],
-    image_path: './img/body.jpg'
-}
+  question: "Wie sehr interessierst du dich für den Menschlichen Körper?",
+  points: [0, 0, 0, 5],
+  image_path: "./img/body.jpg",
+};
 let question2 = {
-    question: "Wie sehr interessierst du dich fürs Programmieren?",
-    points: [5, 4, 1, 0],
-    image_path: './img/programming.jpg'
-}
+  question: "Wie sehr interessierst du dich fürs Programmieren?",
+  points: [5, 4, 1, 0],
+  image_path: "./img/programming.jpg",
+};
 let question3 = {
-    question: "Wie sehr interessierst du dich für Datenbanken?",
-    points: [4, 3, 0, 0],
-    image_path: './img/databases.jpg'
-}
+  question: "Wie sehr interessierst du dich für Datenbanken?",
+  points: [4, 3, 0, 0],
+  image_path: "./img/databases.jpg",
+};
 let question4 = {
-    question: "Wie sehr interessierst du dich für Gesundheit und Krankheiten?",
-    points: [0, 0, 0, 5],
-    image_path: './img/illness.jpg'
-}
+  question: "Wie sehr interessierst du dich für Gesundheit und Krankheiten?",
+  points: [0, 0, 0, 5],
+  image_path: "./img/illness.jpg",
+};
 let question5 = {
-    question: "Wie sehr interessierst du dich für Stromkreise?",
-    points: [3, 3, 5, 2],
-    image_path: './img/circuit.jpg'
-}
+  question: "Wie sehr interessierst du dich für Stromkreise?",
+  points: [3, 3, 5, 2],
+  image_path: "./img/circuit.jpg",
+};
 let question6 = {
-    question: "Wie sehr interessierst du dich für die Hardware deines Computers?",
-    points: [3, 2, 5, 1],
-    image_path: './img/pcHardware.jpg'
-}
+  question: "Wie sehr interessierst du dich für die Hardware deines Computers?",
+  points: [3, 2, 5, 1],
+  image_path: "./img/pcHardware.jpg",
+};
 let question7 = {
-    question: "Wie sehr interessierst du dich für Roboter?",
-    points: [5, 2, 4, 1],
-    image_path: './img/robots.jpg'
-}
+  question: "Wie sehr interessierst du dich für Roboter?",
+  points: [5, 2, 4, 1],
+  image_path: "./img/robots.jpg",
+};
 let question8 = {
-    question: "Wie sehr interessierst du dich für die Automatisierung?",
-    points: [3, 1, 4, 2],
-    image_path: './img/automation.jpg'
-}
+  question: "Wie sehr interessierst du dich für die Automatisierung?",
+  points: [3, 1, 4, 2],
+  image_path: "./img/automation.jpg",
+};
 let question9 = {
-    question: "Wie sehr interessierst du dich für das Web?",
-    points: [3, 5, 0, 0],
-    image_path: './img/web.jpg'
-}
+  question: "Wie sehr interessierst du dich für das Web?",
+  points: [3, 5, 0, 0],
+  image_path: "./img/web.jpg",
+};
 let question10 = {
-    question: "Wie sehr interessierst du dich wie der Funk funktioniert?",
-    points: [2, 0, 5, 0],
-    image_path: './img/wireless.jpg'
-}
+  question: "Wie sehr interessierst du dich wie der Funk funktioniert?",
+  points: [2, 0, 5, 0],
+  image_path: "./img/wireless.jpg",
+};
 let question11 = {
-    question: "Wie gerne arbeitest du an einem Netzwerk?",
-    points: [3, 2, 5, 1],
-    image_path: './img/network.jpg'
-}
+  question: "Wie gerne arbeitest du an einem Netzwerk?",
+  points: [3, 2, 5, 1],
+  image_path: "./img/network.jpg",
+};
 let question12 = {
-    question: "Wie sehr interessierst du dich für IT-Sicherheit",
-    points: [3, 4, 0, 0],
-    image_path: './img/security.jpg'
-}
+  question: "Wie sehr interessierst du dich für IT-Sicherheit",
+  points: [3, 4, 0, 0],
+  image_path: "./img/security.jpg",
+};
 let question13 = {
-    question: "Wie gerne entwickelst du Software?",
-    points: [4, 4, 0, 0],
-    image_path: './img/softwareDevelopment.jpg'
-}
+  question: "Wie gerne entwickelst du Software?",
+  points: [4, 4, 0, 0],
+  image_path: "./img/softwareDevelopment.jpg",
+};
 let question14 = {
-    question: "Wie sehr interessierst du dich für Video- und Audiobearbeitung?",
-    points: [0, 5, 0, 0],
-    image_path: './img/video.jpg'
-}
+  question: "Wie sehr interessierst du dich für Video- und Audiobearbeitung?",
+  points: [0, 5, 0, 0],
+  image_path: "./img/video.jpg",
+};
 let question15 = {
-    question: "Wie gerne arbeitest du mit Micro-Controllern?",
-    points: [0, 2, 5, 0],
-    image_path: './img/micro.jpg'
-}
+  question: "Wie gerne arbeitest du mit Micro-Controllern?",
+  points: [0, 2, 5, 0],
+  image_path: "./img/micro.jpg",
+};
 let question16 = {
-    question: "Wie gerne arbeitest du Physisch an Dingen?",
-    points: [0, 0, 5, 5],
-    image_path: './img/circuit.jpg'
-}
+  question: "Wie gerne arbeitest du Physisch an Dingen?",
+  points: [0, 0, 5, 5],
+  image_path: "./img/circuit.jpg",
+};
 let question17 = {
-    question: "Interessierst du dich für medizinische Gerätschaften?",
-    points: [0, 0, 2, 5],
-    image_path: './img/microSkop.jpg'
-}
+  question: "Interessierst du dich für medizinische Gerätschaften?",
+  points: [0, 0, 2, 5],
+  image_path: "./img/microSkop.jpg",
+};
 let question18 = {
-    question: "Wie sehr möchtest du einmal im Gesundheitswesen arbeiten?",
-    points: [0, 0, 0, 5],
-    image_path: './img/medizin.jpg'
-}
+  question: "Wie sehr möchtest du einmal im Gesundheitswesen arbeiten?",
+  points: [0, 0, 0, 5],
+  image_path: "./img/medizin.jpg",
+};
 
-let questions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question18, question10, question11, question12, question13, question14, question15, question16, question17];
+let questions = [
+  question1,
+  question2,
+  question3,
+  question4,
+  question5,
+  question6,
+  question7,
+  question8,
+  question9,
+  question18,
+  question10,
+  question11,
+  question12,
+  question13,
+  question14,
+  question15,
+  question16,
+  question17,
+];
--- js/script.js
+++ js/script.js
@@ -1,58 +1,57 @@
 let current_question = 0;
 let main_element = document.getElementById("main");
-let points = [1, 1, 1, 1]
+let points = [1, 1, 1, 1];
 let percentages = [
-    {
-        percentage: 0,
-        zweig: "InformatikerIn"
-    },
-    {
-        percentage: 0,
-        zweig: "MedientechnikerIn"
-    },
-    {
-        percentage: 0,
-        zweig: "ElektrotechnikerIn"
-    },
-    {
-        percentage: 0,
-        zweig: "MedizintechnikerIn"
-    },
-
-]
+  {
+    percentage: 0,
+    zweig: "InformatikerIn",
+  },
+  {
+    percentage: 0,
+    zweig: "MedientechnikerIn",
+  },
+  {
+    percentage: 0,
+    zweig: "ElektrotechnikerIn",
+  },
+  {
+    percentage: 0,
+    zweig: "MedizintechnikerIn",
+  },
+];
 let all_points = 0;
 
 let light_mode = {
-    "--backgroundColor": "rgb(236, 236, 236)",
-    "--mainColor1": "rgb(214, 214, 214)",
-    "--mainColor2": "rgb(231, 231, 231)",
-    "--colorScheme": "light",
-    "--boxshadow": "10px 10px 5px 0px rgba(165, 165, 165, 0.75)"
-}
+  "--backgroundColor": "rgb(236, 236, 236)",
+  "--mainColor1": "rgb(214, 214, 214)",
+  "--mainColor2": "rgb(231, 231, 231)",
+  "--colorScheme": "light",
+  "--boxshadow": "10px 10px 5px 0px rgba(165, 165, 165, 0.75)",
+};
 let dark_mode = {
-    "--backgroundColor": "rgb(97, 97, 97)",
-    "--mainColor1": "rgb(61, 61, 61)",
-    "--mainColor2": "rgb(95, 95, 95)",
-    "--colorScheme": "dark",
-    "--boxshadow": "10px 10px 5px 0px rgba(165, 165, 165, 0.75)"
-}
+  "--backgroundColor": "rgb(97, 97, 97)",
+  "--mainColor1": "rgb(61, 61, 61)",
+  "--mainColor2": "rgb(95, 95, 95)",
+  "--colorScheme": "dark",
+  "--boxshadow": "10px 10px 5px 0px rgba(165, 165, 165, 0.75)",
+};
 
 let currently_light_mode = false;
 
-if (localStorage['currently_light_mode']) {
-    currently_light_mode = JSON.parse(localStorage['currently_light_mode']);
+if (localStorage["currently_light_mode"]) {
+  currently_light_mode = JSON.parse(localStorage["currently_light_mode"]);
 } else {
-    localStorage['currently_light_mode'] = JSON.stringify(false);
+  localStorage["currently_light_mode"] = JSON.stringify(false);
 }
 
 setLightMode(currently_light_mode);
 loadQuestion(current_question);
 
 function loadQuestion(question_id) {
-    current_question = question_id;
-    if (question_id < questions.length) {
-        //generate Question
-        main_element.innerHTML = `
+  current_question = question_id;
+  if (question_id < questions.length) {
+    //generate Question
+    main_element.innerHTML = `
         <h1>${questions[question_id].question}</h1>
         <div id="img" style='background-image: url("${questions[question_id].image_path}")'></div>
         <div class="questions_grid">
@@ -70,107 +69,110 @@ function loadQuestion(question_id) {
             </div>
         </div>
     `;
-    } else {
-        //generate endscreen
+  } else {
+    //generate endscreen
 
+    for (let k = 0; k < points.length; k++) {
+      all_points += points[k];
+    }
 
-        for (let k = 0; k < points.length; k++) {
-            all_points += points[k];
+    for (let i = 0; i < points.length; i++) {
+      percentages[i].percentage = (points[i] * 100) / all_points;
+    }
+
+    //BUBBLESORT
+    for (var i = 0; i < percentages.length; i++) {
+      // Last i elements are already in place
+      for (var j = 0; j < percentages.length - i - 1; j++) {
+        // Checking if the item at present iteration
+        // is greater than the next iteration
+        if (percentages[j].percentage > percentages[j + 1].percentage) {
+          // If the condition is true then swap them
+          var temp = percentages[j];
+          percentages[j] = percentages[j + 1];
+          percentages[j + 1] = temp;
         }
+      }
+    }
 
-
-        for (let i = 0; i < points.length; i++) {
-            percentages[i].percentage = points[i] * 100 / all_points;
-        }
-
-        //BUBBLESORT
-        for (var i = 0; i < percentages.length; i++) {
-
-            // Last i elements are already in place 
-            for (var j = 0; j < (percentages.length - i - 1); j++) {
-
-                // Checking if the item at present iteration
-                // is greater than the next iteration
-                if (percentages[j].percentage > percentages[j + 1].percentage) {
-
-                    // If the condition is true then swap them
-                    var temp = percentages[j]
-                    percentages[j] = percentages[j + 1]
-                    percentages[j + 1] = temp
-                }
-            }
-        }
-
-        if (all_points > 4) {
-            main_element.innerHTML = `
-            <h1 onclick='window.open("secret/secret.html")'>Du bist zu ${Math.round(percentages[3].percentage)}% ${percentages[3].zweig}</h1>
-            <h2>${Math.round(percentages[2].percentage)}% ${percentages[2].zweig}</h2>
-            <h2>${Math.round(percentages[1].percentage)}% ${percentages[1].zweig}</h2>
-            <h2>${Math.round(percentages[0].percentage)}% ${percentages[0].zweig}</h2>
+    if (all_points > 4) {
+      main_element.innerHTML = `
+            <h1 onclick='window.open("secret/secret.html")'>Du bist zu ${Math.round(
+              percentages[3].percentage
+            )}% ${percentages[3].zweig}</h1>
+            <h2>${Math.round(percentages[2].percentage)}% ${
+        percentages[2].zweig
+      }</h2>
+            <h2>${Math.round(percentages[1].percentage)}% ${
+        percentages[1].zweig
+      }</h2>
+            <h2>${Math.round(percentages[0].percentage)}% ${
+        percentages[0].zweig
+      }</h2>
             `;
-            document.getElementById("body").style = `background-image: url(./img/${percentages[3].zweig}.jpg)`;
-        } else {
-            main_element.innerHTML = `
+      document.getElementById(
+        "body"
+      ).style = `background-image: url(./img/${percentages[3].zweig}.jpg)`;
+    } else {
+      main_element.innerHTML = `
             <h1 onclick='window.open("secret/secret.html")'>Gaunz a Witziger</h1>
             <h1>Wia entfön da a HAK do san so lustige Leid wie du</h1>
             `;
-        }
-
-
     }
+  }
 }
 
 function answerQuestion(a) {
-    let question_points = questions[current_question].points;
+  let question_points = questions[current_question].points;
 
-    for (let i = 0; i < question_points.length; i++) {
-        switch (a) {
-            case 4:
-                question_points[i] = 0;
-                break;
-            case 3:
-                question_points[i] /= 3;
-                break;
-            case 2:
-                question_points[i] = (question_points[i] / 3) * 2;
-                break;
-            case 1:
-                question_points[i] = question_points[i];
-                break;
-        }
-        points[i] += question_points[i];
+  for (let i = 0; i < question_points.length; i++) {
+    switch (a) {
+      case 4:
+        question_points[i] = 0;
+        break;
+      case 3:
+        question_points[i] /= 3;
+        break;
+      case 2:
+        question_points[i] = (question_points[i] / 3) * 2;
+        break;
+      case 1:
+        question_points[i] = question_points[i];
+        break;
     }
-    console.log(question_points);
-    console.log(points);
-    loadQuestion(current_question += 1);
+    points[i] += question_points[i];
+  }
+  console.log(question_points);
+  console.log(points);
+  loadQuestion((current_question += 1));
 }
 
 function changeLightMode() {
-    currently_light_mode = !currently_light_mode;
-    setLightMode(currently_light_mode);
+  currently_light_mode = !currently_light_mode;
+  setLightMode(currently_light_mode);
 }
 
 function setLightMode(is_light_mode) {
-    let r = document.querySelector(':root');
-    let rs = getComputedStyle(r);
+  let r = document.querySelector(":root");
+  let rs = getComputedStyle(r);
 
-    if (is_light_mode) {
-        console.log("Pain");
+  if (is_light_mode) {
+    console.log("Pain");
 
-        r.style.setProperty('--backgroundColor', light_mode["--backgroundColor"]);
-        r.style.setProperty('--mainColor1', light_mode["--mainColor1"]);
-        r.style.setProperty('--mainColor2', light_mode["--mainColor2"]);
-        r.style.setProperty('--colorScheme', light_mode["--colorScheme"]);
-        r.style.setProperty('--boxshadow', light_mode["--boxshadow"]);
-        colorChanger.src = '/img/darkmodeicon.svg';
-    } else {
-        r.style.setProperty('--backgroundColor', dark_mode["--backgroundColor"]);
-        r.style.setProperty('--mainColor1', dark_mode["--mainColor1"]);
-        r.style.setProperty('--mainColor2', dark_mode["--mainColor2"]);
-        r.style.setProperty('--colorScheme', dark_mode["--colorScheme"]);
-        r.style.setProperty('--boxshadow', dark_mode["--boxshadow"]);
-        colorChanger.src = '/img/lightmodeicon.svg';
-    }
+    r.style.setProperty("--backgroundColor", light_mode["--backgroundColor"]);
+    r.style.setProperty("--mainColor1", light_mode["--mainColor1"]);
+    r.style.setProperty("--mainColor2", light_mode["--mainColor2"]);
+    r.style.setProperty("--colorScheme", light_mode["--colorScheme"]);
+    r.style.setProperty("--boxshadow", light_mode["--boxshadow"]);
+    colorChanger.src = "/img/darkmodeicon.svg";
+  } else {
+    r.style.setProperty("--backgroundColor", dark_mode["--backgroundColor"]);
+    r.style.setProperty("--mainColor1", dark_mode["--mainColor1"]);
+    r.style.setProperty("--mainColor2", dark_mode["--mainColor2"]);
+    r.style.setProperty("--colorScheme", dark_mode["--colorScheme"]);
+    r.style.setProperty("--boxshadow", dark_mode["--boxshadow"]);
+    colorChanger.src = "/img/lightmodeicon.svg";
+  }
 
-    localStorage['currently_light_mode'] = currently_light_mode;
+  localStorage["currently_light_mode"] = currently_light_mode;
 }
